6d26d52a4772e0868c3743b98a86dc26d948dffa
vivekdogra <vivek@credr.in>
2015-12-19 02:03:42 +0530

    sms subscriber for order placements

---
+++ PubSub/pubsub_design_pattern/smssubscriber.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import pika
+import time
+
+
+def subscribe():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='order_exchange',
+                             type='fanout')
+
+    result = channel.queue_declare(exclusive=True)
+    queue_name = result.method.queue
+
+    channel.queue_bind(exchange='order_exchange',
+                       queue=queue_name)
+
+    print ' [*] Waiting for message. To exit press CTRL+C'
+
+    def on_message_send_sms(ch, method, properties, body):
+
+        # mimicking sms execution time with sleep method
+        time.sleep(1.5)
+        print "SMS sent to %r" % body
+        return 1
+
+    channel.basic_consume(on_message_send_sms,
+                          queue=queue_name,
+                          no_ack=True)
+
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    subscribe()
